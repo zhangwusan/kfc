55fe4580a28ef65ee818a6873a2040ec3f123726
Ougi <ougi@Ougis-MacBook-Pro.local>
2026-04-24 19:40:54 +0700

    update kernel

--- src/cobra/core/kernels/builtin.py
+++ src/cobra/core/kernels/builtin.py
@@ -7,45 +7,27 @@ from .base import BaseKernel, KernelFactory
 
 @KernelFactory.register("indicator", "hard")
 class IndicatorKernel(BaseKernel):
-    def __init__(self, alpha=1, beta=0, threshold=0.5):
+    def __init__(self, threshold=0.5):
         super().__init__()
-        self.alpha = alpha
-        self.beta = beta
         self.threshold = threshold
     
-    def __call__(self, dist1, dist2=None):
-        if dist2 is None:
-            score = self.alpha * dist1
-        else:
-            score = self.alpha * dist1 + self.beta * dist2
-        return (score < self.threshold).astype(float)
+    def __call__(self, D):
+        return (D < self.threshold).astype(float)
 
 @KernelFactory.register("rbf", "gaussian")
 class RBFKernel(BaseKernel):
-    def __init__(self, alpha=1, beta=0, gamma=1.0):
+    def __init__(self, gamma=1.0):
         super().__init__()
-        self.alpha = alpha
-        self.beta = beta
         self.gamma = gamma
     
-    def __call__(self, dist1, dist2=None):
-        if dist2 is None:
-            score = self.alpha * dist1
-        else:
-            score = self.alpha * dist1 + self.beta * dist2
-        return np.exp(-self.gamma * score)
+    def __call__(self, D):
+        return np.exp(-self.gamma * D)
 
 @KernelFactory.register("laplace")
 class LaplaceKernel(BaseKernel):
-    def __init__(self, alpha=1, beta=0, gamma=1.0):
+    def __init__(self, gamma=1.0):
         super().__init__()
-        self.alpha = alpha
-        self.beta = beta
         self.gamma = gamma
     
-    def __call__(self, dist1, dist2=None):
-        if dist2 is None:
-            score = self.alpha * dist1
-        else:
-            score = self.alpha * dist1 + self.beta * dist2
-        return np.exp(-self.gamma * np.abs(score))
+    def __call__(self, D):
+        return np.exp(-self.gamma * np.abs(D))
